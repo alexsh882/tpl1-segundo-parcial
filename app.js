1bcdc65943363a5aabeb43decbc6bee389a3aa4f
alexsh882 <alexsh882@gmail.com>
2023-06-21 14:38:24 -0300

    Se configuraron los middlewares, dotenv y el directorio para archivos estáticos

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+const path = require("path");
 
 const app = express();
 
+//configuración del motor de plantillas
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//configuración de variables de entorno
+dotenv.config({ path: ".env" });
+
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan("dev"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+// carpeta para archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use("/api", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log(`Servidor en http://localhost:${process.env.PORT}`));
+  
