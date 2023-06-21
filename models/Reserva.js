d08bc5bc4d36d6cb4efefec37ae3470471276e4a
alexsh882 <alexsh882@gmail.com>
2023-06-21 14:45:56 -0300

    Se creó el modelo de Reserva

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,27 @@
-// TODO: Crear modelo de datos de Reserva
+"use strict";
+
+const { DataTypes, sequelize } = require("../database/config");
+
+const Reserva = sequelize.define(
+  "Reserva",
+  {
+    fullname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    date: { type: DataTypes.DATE },
+  },
+  {
+    sequelize,
+    paranoid: true,
+    modelName: "Reserva",
+    tableName: "reservas",
+    underscored: true,
+  }
+);
+
+module.exports = Reserva;
